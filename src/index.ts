c6f4dbd8fd1b7fc67e6bf937d3a3c1012a7e04db
Guillaume Salicis <gsalicis@d-edge.com>
2022-03-16 11:15:21 +0100

    take scenario name from command line arguments

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,27 @@
 import { parse as yamlParse } from "https://deno.land/std@0.82.0/encoding/yaml.ts";
 import { Scenario } from "./Scenario.ts";
 
-const data = yamlParse(await Deno.readTextFile("scenario.yml")) as any;
+const printUsage = (exitError = true) => {
+  const text =
+    "Usage: deno run --allow-read --allow-net src/index.ts <filename>";
+  (exitError ? console.error : console.info)(text);
+  if (exitError) {
+    Deno.exit(1);
+  }
+};
 
+if (Deno.args.length !== 1) {
+  printUsage();
+}
+
+let fileContent = "";
+try {
+  fileContent = await Deno.readTextFile(Deno.args[0]);
+} catch (e) {
+  console.error(e.message);
+  printUsage();
+}
+
+const data = yamlParse(fileContent) as any;
 const scenario = new Scenario(data);
 await scenario.run();
-// console.log(JSON.stringify(scenario, null, ' '))
