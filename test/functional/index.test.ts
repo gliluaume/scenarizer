081aa95f4cb9ff0a2af972f4fa74877e15202df1
gliluaume <guillaume.salicis@gmail.com>
2023-05-16 13:15:27 +0200

    fixup: having something working

--- test/functional/index.test.ts
+++ test/functional/index.test.ts
@@ -1,11 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.104.0/testing/asserts.ts";
 
-const TIMEOUT = 5000;
-
-const launch = (port: number) => {
-  console.log("launch something");
-  const command = new Deno.Command(Deno.execPath(), {
-    env: { PORT: port.toString()},
+const mockCmd = (port: number) =>
+  new Deno.Command(Deno.execPath(), {
+    env: { PORT: port.toString() },
     args: [
       "run",
       "--allow-read",
@@ -15,57 +12,48 @@ const launch = (port: number) => {
     ],
     stdout: "piped",
     stderr: "piped",
-
   });
 
-  const process = command.spawn()
-  // console.log("Data received:", new TextDecoder().decode(process.stdout.));
-  // console.log("Data received:", process.stdout.getReader());
-  // const joined = mergeReadableStreams(process.stdout, process.stderr);
-
-  // const file = await Deno.open("./process_output.txt", {
-  //   read: true,
-  //   write: true,
-  //   create: true,
-  // });
-
-  // joined.pipeTo(file.writable).then(() => console.log("pipe join done"));
-
-  return process;
-};
-
-const stop = (process: Deno.ChildProcess, delay = TIMEOUT): Promise<boolean> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      process.stdout.cancel();
-      process.stderr.cancel();
-      process.kill();
-      resolve(true);
-    }, delay);
+const testCmd = (tag: string) =>
+  new Deno.Command(Deno.execPath(), {
+    args: [
+      "run",
+      "--allow-hrtime",
+      "--allow-env",
+      "--allow-read",
+      "--allow-net",
+      "--unsafely-ignore-certificate-errors",
+      "src/index.ts",
+      `./test/functional/scenarios/${tag}.yml`,
+    ],
   });
-};
+
+const filterUndeterministic = (text: string) =>
+  text.split("\n").map((line) =>
+    line.match(/^duration: /i)
+      ? line.replace(/(duration: )(\d+\.?\d*)/i, "$1xxx.xxx")
+      : line
+  ).join("\n");
 
 Deno.test("functional test", async (tst) => {
-  const process = await launch(3002);
-  await tst.step("blublu", async () => {
-    const command = new Deno.Command(Deno.execPath(), {
-      args: [
-        "run",
-        "--allow-hrtime",
-        "--allow-env",
-        "--allow-read",
-        "--allow-net",
-        "--unsafely-ignore-certificate-errors",
-        "src/index.ts",
-        "docs/samples/scenario.yml"
-      ],
-    });
+  const tag = "scenario";
+  await tst.step(tag, async () => {
+    const expected = await Deno.readTextFile(
+      `./test/functional/expectations/${tag}.expected`,
+    );
+    const process = await mockCmd(3002).spawn();
+    const command = testCmd(tag);
     const { code, stdout, stderr } = await command.output();
-    console.log("code", code);
-    assertEquals(code, 0);
 
-    console.log("stdout", new TextDecoder().decode(stdout));
-    console.log("stderr", new TextDecoder().decode(stderr));
+    const err = new TextDecoder().decode(stderr);
+    const actual = new TextDecoder().decode(stdout);
+    assertEquals(code, 0);
+    assertEquals(
+      filterUndeterministic(actual),
+      filterUndeterministic(expected),
+    );
+
+    process.stdout.cancel();
+    process.stderr.cancel();
   });
-  await stop(process, 0);
 });
